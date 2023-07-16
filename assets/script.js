c2d2a771bb96efe258dd88961fadbbdbed612f4f
rambriz91 <rambriz91@gmail.com>
2023-07-16 14:48:51 -0700

    added character set variables.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = lowerCase.toString().toUpperCase().split(',');
+var num = ['1','2','3','4','5','6','7','8','9','0'];
+var specialChar = ['!','@','#','$','%','^','&','*','?','<','>','.','-','+','=','?','~'];
+var charList = [];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
